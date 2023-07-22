04819d30f5bcb179b5a677292e6b07aef9903554
cko <carlkomondi@gmail.com>
2023-07-22 10:12:54 -0500

    Created about section

---
+++ src/components/Bisect.svelte
@@ -0,0 +1,20 @@
+<style lang="scss">
+    section.bisect {
+        height: calc(100svh - 4rem);
+        padding: 0px 4vw;
+
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr 1fr;
+        gap: 1rem 1rem;
+
+    }
+</style>
+
+<script>
+    export let id = "";
+</script>
+
+<section id={ id } class="bisect">
+    <slot />
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 
 <script>
+    import About from "../home/about.svelte";
     import Hero from "../home/hero.svelte";
     import Partners from "../home/partners.svelte";
 
 </script>
 
-<main style={ `padding: 0px 4vw` }>
-    <Hero />
-
+<main style={ `padding: 0px 0vw` }>
+    <About />
 </main>
 
