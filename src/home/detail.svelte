494ef3628657c91561fe516822aee663a6e359bb
cko <carlkomondi@gmail.com>
2023-07-22 10:29:56 -0500

    Added guides section

---
+++ src/home/detail.svelte
@@ -0,0 +1,73 @@
+
+
+<style lang="scss">
+    @use "../variables" as app;
+
+    div.graphic {
+        padding: 2rem 0px;
+
+        > img {
+            width: 100%;
+            height: 100%;
+            background-color: pink;
+            border-radius: 1rem;
+        }
+    }
+
+    div.info {
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        p { margin-top: 2rem; }
+
+        > h5 {
+            font-weight: app.$weight-bold;
+            margin-top: 2.5rem;
+            margin-bottom: 0.5rem;
+        }
+
+        div.partners {
+
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            gap: 0.5rem;
+
+            span {
+                width: 2.4rem;
+                height: 2.4rem;
+                background-color: pink;
+                border-radius: 3rem;
+            }
+        }
+    }
+
+    
+</style>
+
+
+<script>
+    import Bisect from "../components/Bisect.svelte";
+
+</script>
+
+
+<Bisect>
+
+    <div class="info">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id accusamus laudantium ad illo aliquid molestiae quaerat ratione mollitia deserunt deleniti, expedita in perferendis asperiores at corrupti porro quis quo vel aperiam voluptatem.</p>
+
+        <h5>Brought to you by:</h5>
+        <div class="partners">
+            <span></span><span></span><span></span>
+        </div>
+
+    </div>
+
+    <div class="graphic">
+        <img src="" alt="">
+    </div>    
+</Bisect>
+
---
+++ src/home/guides.svelte
@@ -0,0 +1,53 @@
+
+
+
+
+
+<style lang="scss">
+    @use "../variables" as app;
+
+    section#guides {
+        position: relative;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+        gap: 1rem 1rem;
+
+        padding: 0px 5vw 7rem 5vw;
+        margin-top: 4rem;
+
+        div {
+            border: 1px solid brown;
+            height: 100px;
+        }
+
+        a {
+            position: absolute;
+            bottom: 2rem;
+            left: 50%;
+
+            transform: translateX(-50%) translateY(0rem);
+        }
+
+        
+    }
+
+</style>
+
+
+<script>
+
+</script>
+
+<section id="guides">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+
+    <a href="/" class="button tertiary">See more</a>
+</section>
+
+
+
