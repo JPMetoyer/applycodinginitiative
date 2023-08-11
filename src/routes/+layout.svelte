61ea186c6a5af5a4a4fb2daa9c7db4180153a371
cko <carlkomondi@gmail.com>
2023-08-10 23:33:58 -0500

    Setup frontend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,17 +16,28 @@
         align-items: center; 
         justify-content: space-between;
 
-        padding: 0.3rem 1rem; 
+        padding: 0.3rem 8vw; 
         background-color: app.$color-background;
         border-bottom: 0.5px dashed app.$color-brand;
 
 
 
         #logo {
-            width: 2.8rem;
             height: 2.8rem;
             fill: app.$color-brand;
             stroke: transparent;
+
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+
+            > h1 {
+                color: app.$color-brand;
+                text-decoration: none;
+                font-weight: app.$weight-black;
+                text-transform: uppercase;
+                font-size: 120%;
+            } 
         }
 
         .links {
@@ -34,7 +45,7 @@
             z-index: 4;
             
             position: fixed;
-            top: 0px;
+            bottom: 0px;
             left: 0px;
             right: 0px;
             height: 3.5rem;
@@ -94,8 +105,10 @@
             bottom: 0px;
             width: 1.2rem;
             height: 1.2rem;
-            fill: app.$color-background;
-            transform: translateY(-50%) rotateZ(180deg);
+            fill: transparent;
+            stroke: app.$color-background;
+            transform: translateY(-50%) rotateZ(315deg);
+            stroke-width: 2;
         }
 
         span {
@@ -132,38 +145,19 @@
 
 <nav>
     <a href="/" id="logo">
-        <svg viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M31.6541 58.8698C49.2384 58.8698 58.8547 44.3016 58.8547 31.6693C58.8547 31.2554 58.8462 30.8436 58.8271 30.4334C60.6936 29.0841 62.3164 27.3999 63.5963 25.4835C61.8834 26.2446 60.0402 26.757 58.1063 26.9881C60.0799 25.8047 61.5956 23.9318 62.3106 21.6996C60.4631 22.795 58.4174 23.591 56.2403 24.0203C54.4951 22.1622 52.0107 21 49.2617 21C43.9817 21 39.7001 25.2816 39.7001 30.559C39.7001 31.3094 39.7843 32.0387 39.9476 32.7388C32.0028 32.3397 24.9575 28.5351 20.2429 22.751C19.4209 24.1634 18.9476 25.8052 18.9476 27.5557C18.9476 30.8722 20.6356 33.8003 23.2022 35.5137C21.634 35.4654 20.1607 35.0341 18.8724 34.3175C18.8703 34.3573 18.8703 34.3976 18.8703 34.4394C18.8703 39.0697 22.1661 42.9353 26.5399 43.8114C25.737 44.0302 24.8923 44.1479 24.0199 44.1479C23.4041 44.1479 22.8053 44.0864 22.2228 43.9757C23.4396 47.7734 26.9692 50.5377 31.1538 50.6156C27.8813 53.1801 23.7592 54.708 19.2804 54.708C18.5093 54.708 17.7483 54.664 17 54.5755C21.2307 57.2868 26.2543 58.8698 31.6541 58.8698Z"/>
-        </svg>            
+        <img src="/icons/wci.png" alt=""> 
+        <h1>The Coding Initiative</h1>         
     </a>
 
-    <div class="links" bind:this={ linksElement } >
-        <a href="/">Why</a>
-        <a href="/">Events</a>
-        <a href="/">Guides</a>
-        <a href="/">Health</a>
-
-        <div class="trailer">
-            <p>Copyright &copy; 2023</p>
-            <p>Afyadux</p>
-        </div>
-    </div>
-
+    
     <span>
-        <a href="/events" id="cta" class="button">
-            Donate
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M11.9934 2C7.34388 2 3.5 5.71789 3.5 10.3178C3.5 12.6357 4.34198 14.7876 5.7305 16.6116C7.24809 18.6235 9.13474 20.3765 11.2704 21.7524C11.8039 22.0944 12.2425 22.0704 12.7285 21.7524C14.8522 20.3765 16.7388 18.6235 18.2695 16.6116C19.657 14.7876 20.5 12.6357 20.5 10.3178C20.5 5.71789 16.6561 2 11.9934 2"/>
+        <a href="https://codinginitiative.org/" target="_blank" id="cta" class="button">
+            Club Page
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.43 5.93005L20.5 12.0001L14.43 18.0701" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M3.5 12H20.33" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
             </svg>
         </a>
-
-        <Icon frame={[ 2, 2 ]} onClick={ () => { linksElement.classList.toggle("show"); }}>
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5 17H19" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M5 12H19" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M5 7H19" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-        </Icon>
     </span>
 </nav>
     
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,433 @@
 
 
 <style lang="scss">
-    main {
-        position: relative;
+    @use "../variables" as app;
+
+    h1 { font-size: 160%; font-weight: app.$weight-black; }
+    p.bold, b { font-weight: app.$weight-bold; }
+
+    span.list {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        > p {
+            padding-left: 1.5rem;
+        }
+    }
+
+    section#contact {
+
+        height: calc(100svh - 5rem);
+        padding: unset;
+
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-columns: auto;
+        gap: 6vw;
+
+        @media screen and (min-width: 960px) {
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr;
+        }
+
+
+        div#canvas {
+
+            position: relative;
+            max-height: calc(100vh - 5rem);
+            min-height: 300px;
+            height: 40vh;
+
+            @media screen and (min-width: 960px) {
+                position: fixed;
+                top: 4rem;
+                width: calc(50vw - 3vw);
+                height: 100vh;
+            }
+
+            > span.check {
+
+                z-index: -2;
+
+                height: 100%;
+
+                display: flex;
+                flex-direction: column;
+                justify-content: space-around;
+
+                span {
+                    height: 1px;
+                    width: 100%;
+                    background-color: app.$color-gray;
+
+                    opacity: 32%;
+                }
+
+                &.horizontal {
+                    width: 100%;
+                    position: absolute; 
+                    flex-direction: row;
+
+                    z-index: -1; 
+
+                    span {
+                        width: 1px;
+                        height: 100%;
+                    };
+
+                    &::after {
+                        content: "";
+                        position: absolute;
+                        top: 0px;
+                        left: 0px;
+                        right: 0px;
+                        bottom: 0px;
+                        background: radial-gradient(50% 50.00% at 50% 50.00%, transparent 0%, app.$color-background 100%);
+                    }
+
+                }
+            }
+        }
+
+        article {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+
+            @media screen and (min-width: 960px) {
+                width: 50%;
+                margin-right: 8vw;
+                margin-left: 50vw;
+            }
+        }
+
+        article > form {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            gap: 0.5rem;
+
+            margin-top: 1rem;
+            margin-bottom: 24vh;
+
+            
+
+            > div { width: 100%; }
+
+
+            > div.description {
+                display: flex; 
+                justify-content: flex-start;
+                flex-direction: column;
+                gap: 0.3rem;
+                margin-bottom: 0.5rem;
+
+                width: 100%;
+            }
+
+            div.prompt {
+                display: flex;
+                flex-direction: column;
+                gap: 1rem;
+
+                > p { font-style: italic; }
+            }
+
+            input[type=radio] { display: none; }
+
+            > div.options {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                gap: 1rem;
+
+                width: 100%;
+            }
+
+            label {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-end;
+                gap: 1rem; 
+                border-radius: 0.8rem;
+                background-color: app.$color-background;
+
+                padding: 1.5rem 1rem 0.65rem 1rem;
+                fill: transparent;
+                width: unset;
+                flex-grow: 1;
+
+                border: 1px dashed app.$color-shade;
+
+                transition-property: box-shadow;
+                transition-duration: 300ms;
+                transition-timing-function: ease-in;
+
+                &.checked {
+                    border: 1px dashed transparent;
+                    box-shadow: 0rem 0rem 1.5rem rgba(40, 42, 54, 0.08);
+
+                    > p { color: app.$color-foreground; }
+                }
+
+                > p { 
+                    text-align: end; 
+                    font-weight: app.$weight-bold;
+                    color: app.$color-gray;
+                }
+
+            }
+
+            > span {
+
+                position: fixed;
+                bottom: 0px;
+                left: 0px;
+                right: 0px;
+                // background-color: app.$color-background;
+                background: linear-gradient(0deg, app.$color-background 40%, transparent);
+
+                padding: 2.5rem 2.4rem 2rem 2.4rem;
+
+                width: 100%;
+                height: 2.5rem;
+
+                display: flex;
+                align-items: center;
+                justify-content: flex-end;
+
+            }
+
+
+            textarea {
+                resize: vertical;
+            }
+
+            input,
+            textarea {
+                padding: 0.6rem 0.8rem;
+                background-color: transparent;
+                border-radius: 0.6rem;
+
+                width: 100%;
+
+
+                border: 1px solid app.$color-shade;
+                &::placeholder { color: app.$color-gray; }
+            }
+        }
 
         
+
+
+        article > div.description {
+            margin-bottom: 1rem;
+
+            display: flex;
+            flex-direction: column;
+            gap: 0.5rem;
+
+            &.header {
+                margin-top: 3rem;
+                margin-bottom: 2rem;
+
+                > p { margin: 0.5rem 0px; }
+            }
+        }
+
+        article > div.options {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr;
+            gap: 1rem 1rem;
+
+            button.item {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-end;
+                gap: 1rem; 
+                border-radius: 0.8rem;
+                background-color: app.$color-background;
+
+                padding: 1.5rem 1rem 0.65rem 1rem;
+                fill: transparent;
+                width: unset;
+                flex-grow: 1;
+
+                border: 1px dashed app.$color-shade;
+
+                transition-property: box-shadow;
+                transition-duration: 300ms;
+                transition-timing-function: ease-in;
+
+                &.checked {
+                    border: 1px dashed transparent;
+                    box-shadow: 0rem 0rem 1.5rem rgba(40, 42, 54, 0.08);
+
+                    > p { color: app.$color-foreground; }
+                }
+
+                > p { 
+                    text-align: end; 
+                    font-weight: app.$weight-bold;
+                    color: app.$color-gray;
+                }
+
+            }
+        }
     }
 </style>
 
-
 <script lang="ts">
-    import Footer from "../elements/footer.svelte";
-    import About from "../home/about.svelte";
-    import Detail from "../home/detail.svelte";
-    import Faq from "../home/faq.svelte";
-    import Guides from "../home/guides.svelte";
-    import Hero from "../home/hero.svelte";
-    import Process from "../home/process.svelte";
+    import { onMount } from "svelte";
+    import Deliverable from "../cards/Deliverable.svelte";
 
+
+
+    let viewportHeight : number = 0;
+
+    $: rows = Math.floor(viewportHeight / 40);
+
+
+    // Answers
+    let streamOption : string; 
+    let name: string;
+    let idNumber: number;
+    let email: string;
+
+    let interest: string;
+    let successfulYear: string;
+    let favoriteCartoon: string;
+    let pineapplePizza: string;
+
+    $: submittable = 
+        (streamOption != undefined) &&
+        (name != "") && (name != undefined) &&
+        (idNumber != null) &&
+        (email != "") && (email != undefined) &&
+
+        (interest != "") && (interest != undefined) &&
+        (successfulYear != "") && (successfulYear != undefined) &&
+        (favoriteCartoon != "") && (favoriteCartoon != undefined) &&
+        (pineapplePizza != "") && (pineapplePizza != undefined)
+        ; 
+
+
+    const submitForm = () => {
+
+    }
 </script>
 
+<svelte:window bind:innerHeight={ viewportHeight } />
 
-<main style={ `padding: 0px 0vw` }>
-    <Hero />
-    <About />
+<main style="padding: 0px 6vw">
+<section id="contact">
 
-    <Guides />
-    <Detail />
-    <Process />
-    <Faq />
+    <div id="canvas">
+        <Deliverable active={ streamOption === "underclassman" } image={ "/images/design.png" } />
+        <Deliverable active={ streamOption === "upperclassman" } image={ "/images/development.png" } />
 
-    <Footer />
+        <span class="check horizontal">
+            { #each Array(rows) as _ }
+            <span></span>
+            {/each }
+        </span>
+
+        <span class="check vertical">
+        { #each Array(rows) as _ }
+            <span></span>
+        {/each }
+        </span>
+    </div>
+
+    <article>
+        <div class="description header">
+            <h1>Wunsche Coding Initiative <br></h1>
+            <h2>2023-2024 Application</h2>
+
+            <p style="margin-bottom: 1rem">
+                Hello Wunsche Scholars, <br>
+                This is the application form for the 2023-2024 Wunsche Coding Initiative cohort. Our mission is to promote interest, exploration, and skill for the field of programming through student-taught lessons at all grade levels.
+                <br>
+                <br>
+                This year, we will be hosting more expeditions for middle school students where members will have a chance to volunteer, increase opportunities for members to compete at all levels, guide members with personal projects, and more on the way.
+            </p>
+
+            <p>A few things to keep in mind: </p>
+            <span class="list">
+                <p>Deadline to submit this application is <b>Aug 14, 2023</b>, since schedules cannot be changed after the first two weeks :(</p>
+                <p class="point">Club meetings will be during StudyHall (2nd for Underclassmen, and 6th for Upperclassmen); if you are a part of a club/class that does activities during your StudyHall, like the Yearbook Club, you will have to choose where you would prefer to spend your time</p>
+                <p class="point">Access to competitions is merit based, meaning that the members chosen to compete will be purely as a factor of skill in coding</p>
+                <p class="point">The cohort resets each year; <b>so previous members also have to apply</b>. This is to give students who are no longer interested an opportunity to go back to their normal scheduling, and new students who are interested a chance to join</p>
+                <p>The club uses a blind selection process, meaning identifying information like name or ID number will be removed from review</p>
+            </span>
+        </div>
+
+        
+        <form on:submit={ submitForm } action="" method="get">
+            <div class="description">
+                <h3>Which stream would you like to join?</h3>
+                <p>Please keep in mind this will be used to change your class schedule and will take effect for the whole school year. <br><br> 9th and 10th will be at Mr. Neal, 11th and 12th at Ms. Gereke</p>
+            </div>
+
+            <input type="radio" bind:group={ streamOption } id="under" name="stream" value="underclassman">
+            <input type="radio" bind:group={ streamOption } id="upper" name="stream" value="upperclassman">
+
+            <div class="options">
+                <label class={ (streamOption === "underclassman") ? "checked" : "" } for="under"><p>9th & 10th<br>Underclassman</p></label>
+                <label class={ (streamOption === "upperclassman") ? "checked" : "" } for="upper"><p>11th & 12th<br>Upperclassman</p></label>
+            </div>
+
+            <br>
+                
+            <div class="description">
+                <h3>Contact information</h3>
+                <p>We will use this information to send updates</p>
+            </div>
+
+            <input bind:value={ name } type="text" placeholder="Your Name" name="name">
+            <input bind:value={ idNumber } pattern="\d*" type="number" name="id" placeholder="ID Number">
+            <input bind:value={ email } type="email" name="email" placeholder="youremail@springisd.students.org">
+
+            <br>
+
+            <div class="description">
+                <h3>General Questions</h3>
+                <p>Information to help us get to know you better - especially in relations to your interest in code. Answer in as many (or few) words as you would like. The more detail you provide, however, the better. Keep in mind that any names, emails, ID numbers or of the sort added to your answers will be edited to "[...]" by a club officer beforehand. If you have to use someone in your answer, use pronouns like: my, them, my friend, my father, my brother etc.</p>
+            </div>
+
+            <div class="prompt">
+                <p>How or why did you find interest in programming. If you have never done it before, and just want to try it out for exploration, you can explain that too</p>
+                <textarea  bind:value={ interest } name="" id="" placeholder="<p>It all started when I watched the Big Bang Theory ...</p>"></textarea>
+            </div>
+
+            <br>
+
+            <div class="prompt">
+                <p>What would a successful year, both personally and socially, as a WCI member look like to you?</p>
+                <textarea name=""  bind:value={ successfulYear } id="" placeholder="<p>I heard (Mr Neal | Ms Gereke) is the GOAT so I wou..</p>"></textarea>
+            </div>
+
+            <br>
+
+            <div class="prompt">
+                <p>Favorite Cartoon show?</p>
+                <textarea  bind:value={ favoriteCartoon } name="" id="" placeholder="<p>So there is this one YouTuber ...</p>"></textarea>
+            </div>
+
+            <br>
+
+            <div class="prompt">
+                <p>Pineapple on Pizza?</p>
+                <textarea  bind:value={ pineapplePizza } name="" id="" placeholder="<p>Only (Taylor's Version)</p>"></textarea>
+            </div>
+
+            <span>
+                <button disabled={ submittable === false } type="submit">{ submittable ? "Submit" : "Fill in all information" }</button>
+            </span>
+        </form>
+    </article>
+
+</section>
 </main>
-
